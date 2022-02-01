b323e4f1e06bc7c5d25e347de47452816cdbe7fd
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2022-02-01 13:06:13 +0100

    restructure the project

---
+++ app/database/routes/client-router.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const route = express.Router()
+
+const services = require('../services/render');
+const clientController = require('../../../app/controller/client-controller');
+
+
+route.get('/', services.homeRoutes);
+
+
+route.get('/add-user', services.add_user)
+
+
+route.get('/update-user', services.update_user)
+
+
+// API
+route.post('/', clientController.create);
+route.get('/', clientController.find);
+route.put('/:id', clientController.update);
+route.delete('/:id', clientController.delete);
+
+
+
+module.exports = route
