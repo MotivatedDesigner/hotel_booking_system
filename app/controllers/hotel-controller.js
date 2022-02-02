28fec8f61dd8dae64785599b499f6f100e666b04
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2022-02-02 10:24:58 +0100

    refactore get hotel-controller

--- app/controllers/hotel-controller.js
+++ app/controllers/hotel-controller.js
@@ -1,53 +1,51 @@
-const Hotel = require("../model/hotel");
+const { hotelModel } = require("../models")
 
-const createHotel = async (req, res) => {
+module.exports = {
+  get,
+  // getAll,
+  // create,
+  // update,
+  // remove
+}
+
+async function get(req, res) {
   try {
-    const hotel = await Hotel.create(req.body);
-    res.send(hotel);
+    const hotel = await hotelModel.findById(req.params.id)
+    res.send(hotel)
+  } catch (error) { res.send(error) }
+}
+
+/* const createHotel = async (req, res) => {
+  try {
+    const hotel = await Hotel.create(req.body)
+    res.send(hotel)
   } catch (error) {
-    res.send(error);
+    res.send(error)
   }
-};
+}
 const updateHotel = async (req, res) => {
   try {
-    await Hotel.findOneAndUpdate({ _id: req.params.id }, req.body);
-    const hotel = await Hotel.find({ _id: req.params.id });
-    res.send(hotel);
+    await Hotel.findOneAndUpdate({ _id: req.params.id }, req.body)
+    const hotel = await Hotel.find({ _id: req.params.id })
+    res.send(hotel)
   } catch (error) {
-    res.send(error);
+    res.send(error)
   }
-};
+}
 const readAllHotel = async (req, res) => {
   try {
-    const hotel = await Hotel.find({});
-    res.send(hotel);
+    const hotel = await Hotel.find({})
+    res.send(hotel)
   } catch (error) {
-    res.send(error);
+    res.send(error)
   }
-};
-const readHotel = async (req, res) => {
-  try {
-    const hotel = await Hotel.findOne({ _id: req.params.id });
-    res.send(hotel);
-  } catch (error) {
-    res.send(error);
-  }
-};
+}
 
 const deleteHotel = async (req, res) => {
   try {
-    await Hotel.deleteOne({ _id: req.params.id });
-    res.send("is deleted");
+    await Hotel.deleteOne({ _id: req.params.id })
+    res.send("is deleted")
   } catch (error) {
-    res.send(error);
+    res.send(error)
   }
-};
-
-module.exports = {
-  createHotel,
-  updateHotel,
-  readAllHotel,
-  deleteHotel,
-  readHotel,
-  verifyToken,
-};
+} */
