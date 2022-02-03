826ba0ef57c1ecadcd4088d6608433d47738584d
hamza elglaoui <chyoin188@gmail.com>
2022-02-03 10:36:57 +0100

    add room reference to hotels

--- app/models/room-model.js
+++ app/models/room-model.js
@@ -5,6 +5,7 @@ const roomSchema = mongoose.Schema(
     type: {
       type: String,
       required: [true, "the type field is required"],
+    //   enum: ["Single", "Double" , "Triple","Suite"],
       minlength: [3, "type must be greater than 3 charachter"],
       maxlength: [24, "type must be less than 24 charachter"],
     },
@@ -13,11 +14,11 @@ const roomSchema = mongoose.Schema(
       required: [true, "the price field is required"],
       maxlength: [100, "price must be less than 100 charachter"],
     },
-    // image: {
-    //   type: String,
-    //   required: true,
-    //   min: 8,
-    // },
+    image: String,
+    hotel_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'hotels'
+    }
   },
   { timestamps: true }
 );
