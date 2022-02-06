53f20b82b715f61db320e78ee7ff0487d4ee5e1a
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2022-02-06 12:03:39 +0100

    adding room-router to routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,9 @@
 const userRouter = require('./user-router')
 const hotelRouter = require('./hotel-router')
+const roomRouter = require('./room-router')
 
 module.exports = {
+  roomRouter,
   hotelRouter,
   userRouter
 }
