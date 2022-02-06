41d9bdb73f9c0e1921b3858d79dd0937af995e87
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2022-02-06 12:04:44 +0100

    adding room-controller to controllers

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,9 @@
 const userController = require('./user-controller')
 const hotelController = require('./hotel-controller')
+const roomController = require('./room-controller')
 
 module.exports = {
+  roomController,
   hotelController,
   userController,
 }
