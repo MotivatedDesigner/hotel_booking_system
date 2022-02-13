6b45bc9bd31d71220112a26826403df16dc37cc4
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2022-02-13 20:30:08 +0100

    applying authentication & authaurization to hotel resource

--- app/middlewares/index.js
+++ app/middlewares/index.js
@@ -1,7 +1,9 @@
 const cookieParser = require('./cookie-parser')
 const isAuth = require('./isAuth')
+const is = require('./is')
 const upload= require('./uploadPhotos')
 module.exports = {
+    is,
     isAuth,
     cookieParser,
     upload
--- app/routes/hotel-router.js
+++ app/routes/hotel-router.js
@@ -1,13 +1,32 @@
 const route = require("express").Router()
 
 const { hotelController } = require("../controllers")
-const { upload ,allowed, authenticated } = require("../middlewares")
+
+const { upload , isAuth, is } = require("../middlewares");
+
 route.get("/find", hotelController.findByNameAndType);
-route.get("/annonce", hotelController.annonce);
 route.get("/", hotelController.getAll);
 route.get("/:id", hotelController.get);
-route.post("/", upload.array("image"), hotelController.create);
-route.patch("/:id",  hotelController.update);
-route.delete("/:id",  hotelController.remove);
+
+route.get("/annonce", 
+  [isAuth, is('admin', 'owner')],
+  hotelController.annonce
+)
+
+route.post("/",
+  [isAuth, is('admin', 'owner')],
+  upload.array("image"), 
+  hotelController.create
+);
+
+route.patch("/:id",
+  [isAuth, is('admin', 'owner')], 
+  hotelController.update
+)
+
+route.delete("/:id",
+  [isAuth, is('admin', 'owner')], 
+  hotelController.remove
+)
 
 module.exports = route;
