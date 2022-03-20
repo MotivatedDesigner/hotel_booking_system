33dcde700f8dbcdfa2719183c1fa0f5445444e4e
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2022-03-20 17:10:02 +0100

    back: hotel-model modification

--- backend/app/models/hotel-model.js
+++ backend/app/models/hotel-model.js
@@ -6,39 +6,37 @@ module.exports = mongoose.model(
     name: {
       type: String,
       required: [true, "the name field is required"],
-      minlength: [3, "name must be greater than 3 charachter"],
-      maxlength: [24, "name must be less than 24 charachter"],
+      minlength: [3, "name must be greater than 3 character"],
+      maxlength: [24, "name must be less than 24 character"],
     },
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'users'
-    },
+    // user: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: 'users'
+    // },
     city: {
       type: String,
       required: [true, "the city field is required"],
-      minlength: [3, "city must be greater than 3 charachter"],
-      maxlength: [24, "city must be less than 24 charachter"],
+      minlength: [3, "city must be greater than 3 character"],
+      maxlength: [24, "city must be less than 24 character"],
     },
-    // adresse: {
-    //   type: String,
-    //   required: [true, "the address field is required"],
-    //   maxlength: [100, "address must be less than 100 charachter"],
-    // },
-    // phone: {
-    //   type: String,
-    //   required: [true, "the phone field is required"],
-    //   minlength: [8, "phone must be greater than 8charachter"],
-    //   maxlength: [24, "phone must be less than 24 charachter"],
-    // },
-    // stars: {
-    //   type: Number,
-    //   required: [true, "the stars field is required"],
-    //   maxlength: [100, "stars must be less than 100 charachter"],
-    // },
-    // image: {
-    //   type: String,
-    //   required: true,
-    //   min: 8,
-    // },
+    address: {
+      type: String,
+      required: [true, "the address field is required"],
+      maxlength: [100, "address must be less than 100 characters"],
+    },
+    phone: {
+      type: String,
+      required: [true, "the phone field is required"],
+      minlength: [8, "phone must be greater than 8 characters"],
+      maxlength: [24, "phone must be less than 24 characters"],
+    },
+    stars: {
+      type: Number,
+      enum: [1, 2, 3, 4, 5],
+      default: 1,
+    },
+    image: {
+      type: String,
+    },
   }, { timestamps: true })
 )
