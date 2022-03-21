7a7c38e5184c18b2beb36520a6b82ef1603d4712
Hamza ELFOUNIR <77061661+hamzaelfounir@users.noreply.github.com>
2022-03-21 10:29:19 +0100

    commit

--- app/config/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  JWT_SECRET: 'sdfsdfsdf',
-  NODE_ENV: 'development'
-}
--- app/database/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-module.exports = () => {
-  mongoose
-    .connect("mongodb://localhost:27017/hotel-booking", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }).catch(err => console.log(err))
-}
